70c0780df2b9cca44d7eb79e5579f13d55fefe04
kangwon.lee.edu@gmail.com <>
2021-05-28 06:02:32 +0000

    separate map() example

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+# lp3thw p.142
+
 s = (1, 'a', 3, 4)
 
-for i in map(lambda x: x*2, s):
-  print("i =", i)
+for i in s:
+  print('i =', i)
+
+
+print("=" * 10)
+
+def f(y):
+  return y * 10
+
+for j in map(f, s):
+  print("j =", j)
+
+
+print("=" * 10)
+
+for k in map(lambda x: x*10, s):
+  print("k =", k)
